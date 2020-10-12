b0566f510fc73ae3802a154391a2a12449210415
Chris Bourke <cbourke@cse.unl.edu>
2020-10-11 20:42:12 -0500

    added connection parameters that are (potentially) necessary for cse configuration

--- src/main/java/unl/cse/albums/DatabaseInfo.java
+++ src/main/java/unl/cse/albums/DatabaseInfo.java
@@ -2,8 +2,13 @@ package unl.cse.albums;
 
 public class DatabaseInfo {
 
+	/**
+	 * Connection parameters that are necessary for CSE's configuration
+	 */
+	public static final String PARAMETERS = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
 	public static final String USERNAME = "login";
 	public static final String PASSWORD = "password";
-	public static final String URL = "jdbc:mysql://cse.unl.edu/" + USERNAME;
-	
+	public static final String URL = "jdbc:mysql://cse.unl.edu/" + USERNAME + PARAMETERS;
+
 }
