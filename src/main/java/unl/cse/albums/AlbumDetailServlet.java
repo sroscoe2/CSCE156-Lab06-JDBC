cc6da9724bbe8fb3023d284c7339d861a312a1f9
Chris Bourke <cbourke@cse.unl.edu>
2020-10-11 19:25:08 -0500

    remove unused import

--- src/main/java/unl/cse/albums/AlbumDetailServlet.java
+++ src/main/java/unl/cse/albums/AlbumDetailServlet.java
@@ -2,7 +2,6 @@ package unl.cse.albums;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
